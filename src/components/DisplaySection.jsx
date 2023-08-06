25df1057d57cd1603279898a234e83e599119e5d
Monique Shaqiri <artista@moniques-air.home>
2023-08-06 08:21:20 +0200

    pushing changes

---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,13 @@
+function DisplaySection() {
+    return ( 
+        <div className="display-section wrapper">
+            <h2 className="title">New</h2>
+            <p className="text">Brillant.</p>
+            <span className="description"> A Display that`s up 2x brighter than the Sun.</span>
+            <button className="button"> Try me</button>
+            <button className="back-button"> TOP</button>
+        </div>
+    );
+}
+
+export default DisplaySection;
