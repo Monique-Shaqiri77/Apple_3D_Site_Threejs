cdc3d7c87e01c783899fb6d100fcc588d0c1c9ee
Monique Shaqiri <artista@moniques-air.home>
2023-08-06 11:10:02 +0200

    push

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -1,13 +1,25 @@
-function DisplaySection() {
-    return ( 
-        <div className="display-section wrapper">
-            <h2 className="title">New</h2>
-            <p className="text">Brillant.</p>
-            <span className="description"> A Display that`s up 2x brighter than the Sun.</span>
-            <button className="button"> Try me</button>
-            <button className="back-button"> TOP</button>
-        </div>
-    );
-}
+import React from 'react';
+
+const DisplaySection = ({ triggerPreview }) => {
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <div className='display-section wrapper'>
+      <h2 className='title'>New</h2>
+      <p className='text'>Brilliant</p>
+      <span className='description'>
+        A display that's up to 2x brighter in the sun.
+      </span>
+      <button className='button' onClick={triggerPreview}>
+        Try me!
+      </button>
+      <button className='back-button' onClick={handleScrollToTop}>
+        TOP
+      </button>
+    </div>
+  );
+};
 
 export default DisplaySection;
--- src/components/SoundSection.jsx
+++ src/components/SoundSection.jsx
@@ -1,18 +1,27 @@
-function SoundSection() {
+
+import React from 'react';
+
+const SoundSection = () => {
+  const handleLearnMore = () => {
+    window.scrollTo(0, document.body.scrollHeight);
+  };
+
   return (
-    <div className="sound-section wrapper">
-      <div className="body">
-        <div className="sound-section-content content">
-          <h2 className="title"> New Sound System</h2>
-          <p className="text"> Feel the Base</p>
-          <span className="description">
-            $From 41.62/mo. for 24 mo. $999 before trade-in
+    <div className='sound-section wrapper'>
+      <div className='body'>
+        <div className='sound-section-content content'>
+          <h2 className='title'>New Sound System</h2>
+          <p className='text'>Feel the base</p>
+          <span className='description'>
+            From $41.62/mo. for 24 mo. or $999 before trade-in
           </span>
-          <ul className="links">
+          <ul className='links'>
             <li>
-              <button className="button">Buy</button>
-              <a href="#" className="link">
-                Learn more
+              <button className='button'>Buy</button>
+            </li>
+            <li>
+              <a className='link' onClick={handleLearnMore}>
+                Learn More
               </a>
             </li>
           </ul>
@@ -20,6 +29,6 @@ function SoundSection() {
       </div>
     </div>
   );
-}
+};
 
 export default SoundSection;
